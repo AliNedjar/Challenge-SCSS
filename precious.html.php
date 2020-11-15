72cfd37379c1662b9c1083dea69e29d015e7f186
AliNedjar <alinedjarali45@outlook.com>
2020-11-15 13:36:47 +0100

    first commit

---
+++ precious.html.php
@@ -0,0 +1,61 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Lord of the ring</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+          integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+
+</head>
+
+<body>
+<nav>
+    <div class="navbar">
+        <p>FiND THE PRCIOUS</P>
+        <h1> Fellows |</h1>
+        <h1> The ring |</h1>
+        <h1> Reward |</h1>
+        <h1> Hunters |</h1>
+        <h1> Join |</h1>
+        <h1> Contact |</h1>
+    </div>
+</nav>
+<div class="container-fluid mt-5 flex-column justify-content-between">
+    <div class="row justify-content-md-center">
+        <div class="ring1and2">
+        </div>
+        <h1 class="text-center">My precious</h1>
+        <div class="ring1and2">
+        </div>
+    </div>
+    <div class="row justify-content-center">
+        <div class="ring3">
+        </div>
+        <h2 class="align-self-center">The one ring</h2>
+    </div>
+    <div class="row justify-content-center">
+        <div class=" d-flex flex-row justify-content-center">
+            <div class="the6Rings align-self-center"></div>
+            <div class="the6Rings align-self-center"></div>
+            <div class="the6Rings align-self-center"></div>
+            <div class="the6Rings align-self-center"></div>
+            <div class="the6Rings align-self-center"></div>
+            <div class="the6Rings align-self-center"></div>
+        </div>
+    </div>
+    <div class="row justify-content-center">
+        <h2 class="align-self-center">Please find it !</h2>
+    </div>
+</div>
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+        crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx"
+        crossorigin="anonymous"></script>
+</body>
+</html>
